1de30f8bbc7b85d661a3a7fd2197da742a4c9be0
JhersonLopez <jherson.lopez88@gmail.com>
2021-06-27 19:06:01 -0500

    probando

--- resources/views/livewire/live-role-table.blade.php
+++ resources/views/livewire/live-role-table.blade.php
@@ -20,7 +20,7 @@
                         </th>
                         <th scope="col"
                             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Usos de Roles x Usuarios
+                            Usos de Roles x Usuarios vila gey
                         </th>
 
                         <th scope="col" class="relative px-6 py-3">
